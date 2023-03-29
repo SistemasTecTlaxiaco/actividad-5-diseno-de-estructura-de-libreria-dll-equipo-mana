7d3415a508e64d945b08cb3c48eae03f5b780783
Jeanette Arlet Salazar Nicolas <arletnicol04@gmail.com>
2023-03-28 20:32:22 -0600

    Agregue Practica 4

---
+++ Consolapractica4/Program.cs
@@ -0,0 +1,49 @@
+﻿using Practica4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolapractica4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba el desayuno que desea ordenar:");
+            String desayunos=Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Escriba la pasta,antojo o ensalada que desea ordenar:");
+            String pastas_antojos_ensaladas = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Escriba el pollo o hamburguesa que desea ordenar:");
+            String pollo_hamburguesa = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Escriba la carne que desea ordenar:");
+            String carnes = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Escriba el platillo del mar que desea ordenar:");
+            String del_mar = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Escriba el caldo o snack que desea ordenar:");
+            String caldos_snack = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Escriba el postre que desea ordenar:");
+            String postres = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Escriba la bebida que desea ordenar:");
+            String bebidas = Convert.ToString(Console.ReadLine());
+            String d = Ordenar.Orden1(desayunos);
+            String p = Ordenar.Orden2(pastas_antojos_ensaladas);
+            String ph = Ordenar.Orden3(pollo_hamburguesa);
+            String c = Ordenar.Orden4(carnes);
+            String dm = Ordenar.Orden5(del_mar);
+            String cs= Ordenar.Orden6(caldos_snack);
+            String po = Ordenar.Orden7(postres);
+            String be = Ordenar.Orden8(bebidas);
+            Console.WriteLine("El desayuno que ordenó es: " + d);
+            Console.WriteLine("La pasta_antojo o ensalada que ordenó es: " + p);
+            Console.WriteLine("El pollo o hamburguesa que ordenó es: " + ph);
+            Console.WriteLine("La carne que ordenó es: " + c);
+            Console.WriteLine("Del mar que ordenó es: " + dm);
+            Console.WriteLine("El caldo o snack que ordenó es: " + cs);
+            Console.WriteLine("El postre que ordenó es: " + po);
+            Console.WriteLine("La bebida que ordenó es: " + be);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Practica4/Ordenar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Practica4
+{
+    public class Ordenar
+    {
+        public static String Orden1(String desayunos)
+        {
+            return desayunos;
+
+        }
+        public static String Orden2(String pastas_antojos_ensaladas)
+        {
+            return pastas_antojos_ensaladas;
+
+        }
+        public static String Orden3(String pollo_hambuerguesas)
+        {
+            return pollo_hambuerguesas;
+
+        }
+        public static String Orden4(String carnes)
+        {
+            return carnes;
+
+        }
+        public static String Orden5(String del_mar)
+        {
+            return del_mar;
+
+        }
+        public static String Orden6(String caldos_snack)
+        {
+            return caldos_snack;
+
+        }
+        public static String Orden7(String postres)
+        {
+            return postres;
+
+        }
+        public static String Orden8(String bebidas)
+        {
+            return bebidas;
+
+        }
+
+    }
+}
